61cffb963f67b4ce49d65e98552813c29016079c
Leandro Pedroso <lrodriguespedroso@hotmail.com>
2019-10-20 16:42:08 -0300

    autenticacao, rotas, ngclass, ngmodel e formulario de login

--- QuickBuy.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ QuickBuy.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+    selector: 'app-nav-menu',
+    templateUrl: './nav-menu.component.html',
+    styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
-  isExpanded = false;
+    isExpanded = false;
 
-  collapse() {
-    this.isExpanded = false;
-  }
+    constructor(private router: Router) {
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
-  }
+    }
+
+    collapse() {
+        this.isExpanded = false;
+    }
+
+    toggle() {
+        this.isExpanded = !this.isExpanded;
+    }
+
+    public usuarioLogado(): boolean {
+        return sessionStorage.getItem("usuario-autenticado") == "1";
+    }
+
+    sair() {
+        sessionStorage.setItem("usuario-autenticado", "0");
+        this.router.navigate(['/']);
+    }
 }
--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,31 @@
-﻿
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { Usuario } from "../../../model/usuario";
+
+
+@Component({
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"]
+})
+
+export class LoginComponent implements OnInit {
+    public usuario;
+    public returnUrl: string;
+
+    constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+        this.usuario = new Usuario();
+    }
+
+    entrar() {
+        if (this.usuario.email == "leandro@teste.com.br" && this.usuario.senha == "abc123") {
+            sessionStorage.setItem("usuario-autenticado", "1");
+            this.router.navigate([this.returnUrl]);
+        }
+    }
+}
